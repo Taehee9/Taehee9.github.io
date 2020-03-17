86db33c2dc8bcbd6bd9c4e79568dc1fbcd21b7f2
Taehee9 <dhstoalfh9509@naver.com>
2020-03-17 18:12:36 +0900

    add favicon

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
+import favicon from '../../pages/favicon.ico'
 
 class Layout extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Layout extends React.Component {
         <Helmet defaultTitle="----- 성장일기 -----" >
           <meta name="google-site-verification" content="MoyzIoUxkZM-Ji6x7WaqoKwFqu9rOcncf1nu7UDvvFo" />
           <script data-ad-client="ca-pub-4193018843260700" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <link rel="shortcut icon" href={favicon} />
         </Helmet>
         {children}
       </div>
